1425b7fec118c71d361bcec5279877969cd869f3
AlenReji <71674586+AlenReji@users.noreply.github.com>
2020-12-07 01:30:13 -0500

    Update ip-config-report.ps1

--- Powershell/ip-config-report.ps1
+++ Powershell/ip-config-report.ps1
@@ -1,1 +1,1 @@
-Get-CimInstance win32_networkadapterconfiguration -Filter ipenabled=true | select Description,Index,IPAddress,IPsubnet,DNSDomain,DNSServerSearchOrder
+Get-CimInstance win32_networkadapterconfiguration -Filter ipenabled=true | Format-Table -GroupBy select Index,Description,IPAddress,IPsubnet,DNSDomain,DNSServerSearchOrder 
